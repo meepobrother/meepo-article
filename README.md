bd8524679186685dbe809a6e9fe997f552c3a767
15138730370 <1037483576@qq.com>
2018-01-05 09:52:27 +0800

    save to back

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 ## qrcode for angular
+```ts
+import { ArticleListInter } from 'meepo-article';
+export class AppComponent {
+  itmes: ArticleListInter[] = [{
+    desc: '考上就“偷着乐”的五类专业, 20年后也不会“衰落”!',
+    images: [
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+    ],
+    avatar: 'https://q.qlogo.cn/g?b=qq&k=VIE5RHCJLvbeXSZXXndASw&s=100',
+    nickname: '杨明明',
+    goods: 0,
+    discuss: 0,
+    look: 0,
+    title: '米波网络',
+    color: 'red',
+    link: ''
+  }, {
+    desc: '考上就“偷着乐”的五类专业, 20年后也不会“衰落”!',
+    images: [
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+    ],
+    avatar: 'https://q.qlogo.cn/g?b=qq&k=VIE5RHCJLvbeXSZXXndASw&s=100',
+    nickname: '杨明明',
+    goods: 0,
+    discuss: 0,
+    look: 0,
+    title: '米波网络',
+    color: 'red',
+    link: ''
+  }];
+}
+
+```
+
 ```html
-<div [qrcode]="_qrcode"></div>
+<article-list [items]="itmes"></article-list>
 ```
--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, ChangeDetectionStrategy,
 } from '@angular/core';
-
+import { ArticleListInter } from '../../src/app/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,5 +9,35 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  
+  itmes: ArticleListInter[] = [{
+    desc: '考上就“偷着乐”的五类专业, 20年后也不会“衰落”!',
+    images: [
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+    ],
+    avatar: 'https://q.qlogo.cn/g?b=qq&k=VIE5RHCJLvbeXSZXXndASw&s=100',
+    nickname: '杨明明',
+    goods: 0,
+    discuss: 0,
+    look: 0,
+    title: '米波网络',
+    color: 'red',
+    link: ''
+  }, {
+    desc: '考上就“偷着乐”的五类专业, 20年后也不会“衰落”!',
+    images: [
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+      'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+    ],
+    avatar: 'https://q.qlogo.cn/g?b=qq&k=VIE5RHCJLvbeXSZXXndASw&s=100',
+    nickname: '杨明明',
+    goods: 0,
+    discuss: 0,
+    look: 0,
+    title: '米波网络',
+    color: 'red',
+    link: ''
+  }];
 }
