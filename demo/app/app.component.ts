c19534da878235c046d1e2c153fcb81ac2f22125
15138730370 <1037483576@qq.com>
2018-01-05 12:44:53 +0800

    save to back

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -49,6 +49,30 @@ export class AppComponent {
     title: '米波网络',
     color: 'red',
     link: ''
+  }, {
+    desc: '考上就“偷着乐”的五类专业, 20年后也不会“衰落”!',
+    images: [
+      {
+        src: 'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+        type: 'image'
+      },
+      {
+        src: 'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+        type: 'image'
+      },
+      {
+        src: 'https://gpic.qpic.cn/gbar_pic/SMBRdKzfPmiaJsG7tzfXFeYKicBIUzEzPwXgXtibQUJiaCRmFYbroVOumWCSRtlI9kiaP/320',
+        type: 'image'
+      },
+    ],
+    avatar: 'https://q.qlogo.cn/g?b=qq&k=VIE5RHCJLvbeXSZXXndASw&s=100',
+    nickname: '杨明明',
+    goods: 0,
+    discuss: 0,
+    look: 0,
+    title: '米波网络',
+    color: 'red',
+    link: ''
   }];
 
   charms: CharmItemInter[] = [{
